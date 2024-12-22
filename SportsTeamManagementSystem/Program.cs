fd091bb6a3db28c92b53f8443c02047eb9ab95e3
szymon <mail@example.com>
2024-12-22 17:34:48 +0100

    Wykonanie klasy Player i Team

--- SportsTeamManagementSystem/Program.cs
+++ SportsTeamManagementSystem/Program.cs
@@ -1,3 +1,42 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+public class Player
+{
+    public string Name {get; set;}
+    public string Position {get; set;}
+    public int Score {get; set;}
+
+    public void updateScore(int newScore)
+    {
+        Score = newScore;
+    }
+}
+
+public class Team
+{
+    public List<Player> players = new List<Player>();
+
+    public void addPlayer(Player player)
+    {
+        players.Add(player);
+    }
+    
+    public void removePlayer(Player player)
+    {
+        players.Remove(player);
+    }
+
+    public void displayPlayersStatistics()
+    {
+        foreach (var player in players)
+        {
+            Console.WriteLine($"Imie: {player.Name}, Pozycja: {player.Position}, Wynik: {player.Score}");
+        }
+    }
+
+    public double Avg()
+    {
+        return players.Average(player => player.Score);
+    }
+    
+}
--- SportsTeamManagementSystem/obj/Debug/net9.0/SportsTeamManagementSystem.AssemblyInfo.cs
+++ SportsTeamManagementSystem/obj/Debug/net9.0/SportsTeamManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsTeamManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d218e5fcf59f1af4aefb028acdf810c9379ea3d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsTeamManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsTeamManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
